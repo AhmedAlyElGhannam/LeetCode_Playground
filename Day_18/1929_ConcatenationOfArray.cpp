621a2df1132d7c72b7aff1c3503b771ac57753b5
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2025-04-26 05:56:17 +0300

    added day 18 problem

---
+++ Day_18/1929_ConcatenationOfArray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> res;
+        
+        res.reserve(nums.size() * 2);
+        
+        res.insert(res.end(), nums.begin(), nums.end());
+        res.insert(res.end(), nums.begin(), nums.end());
+
+        return res;
+    }
+};
+
+
